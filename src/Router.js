101a35b7f406e6139655b99757b299b5c72a0e94
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-02-18 10:37:28 +0900

    fix:컨플릭트 해결
    - Refactor:리뷰반영
    
    - Refactor:리뷰수정
    
    refactor:리뷰반영
    
    refactor:리뷰반영

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import ClassDetail from './pages/ClassDetail/ClassDetail';
 import Creator from './pages/Creator/Creator';
+import ProductsDetail from './pages/ProductsDetail/ProductsDetail';
+import CreatorMain from './pages/Creator/CreatorMain/CreatorMain';
+import CreatorMypage from './pages/Creator/CreatorMypage/CreatorMypage';
+import Edit from './pages/Creator/CreatorMypage/Product/Edit/Edit';
 import Nav from './components/Nav/Nav';
 import Search from './pages/Search/Search';
 import Login from './pages/Login/Login';
@@ -20,6 +24,10 @@ const Router = () => {
         <Route path="/search" element={<Search />} />
         <Route path="/login" element={<Login />} />
         <Route path="/oauth/callback/kakao" element={<Auth />} />
+        <Route path="/products" element={<ProductsDetail />} />
+        <Route path="/creator" element={<CreatorMain />} />
+        <Route path="/creator/mypage" element={<CreatorMypage />} />
+        <Route path="/creator/mypage/edit/*" element={<Edit />} />
       </Routes>
       <Footer />
     </BrowserRouter>
