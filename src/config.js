a26d616f8623019e0b070c2ca8d1ea6de24e8109
seoltang <yong_drg@naver.com>
2022-02-21 16:09:19 +0900

    Add: 공용 파일 수정 - theme.js: flex 함수 추가 (PR 리뷰 반영) - config.js: API 변수 추가

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'http://10.58.62.159:8000';
+
+export const api = {
+  login: BASE_URL + '/users/callback',
+  products: BASE_URL + '/products',
+  reviews: BASE_URL + '/reviews',
+};
