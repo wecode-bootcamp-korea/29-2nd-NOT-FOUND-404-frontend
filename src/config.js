9a39f9ba3f7a27355f0c1a8c9b5b0dbca098f096
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-02-22 15:42:10 +0900

    [공통] config.js API 수정 및 env 추가

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
-export const BASE_URL = 'http://10.58.62.159:8000';
+export const BASE_URL = 'http://10.58.3.139:3000';
 
 export const api = {
   login: BASE_URL + '/users/callback',
   products: BASE_URL + '/products',
   reviews: BASE_URL + '/reviews',
+  search: BASE_URL + '/search',
+  redirect_uri: 'http://localhost:3000/oauth/callback/kakao',
+  kakao_token_request: BASE_URL + '/users/callback',
 };
