78e3efd1ec19e4fd8e81ac5bea2d757c5c0b1f0a
seoltang <yong_drg@naver.com>
2022-02-24 16:56:37 +0900

    Fix: conflict resolve

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import ClassDetail from './pages/ClassDetail/ClassDetail';
-import Creator from './pages/Creator/Creator';
-import ProductsDetail from './pages/ProductsDetail/ProductsDetail';
-import CreatorMain from './pages/Creator/CreatorMain/CreatorMain';
-import CreatorMypage from './pages/Creator/CreatorMypage/CreatorMypage';
-import Edit from './pages/Creator/CreatorMypage/Product/Edit/Edit';
 import Nav from './components/Nav/Nav';
 import Search from './pages/Search/Search';
 import Login from './pages/Login/Login';
@@ -20,14 +15,9 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/class/:classId" element={<ClassDetail />} />
-        <Route path="/creator" element={<Creator />} />
         <Route path="/search" element={<Search />} />
         <Route path="/login" element={<Login />} />
         <Route path="/oauth/callback/kakao" element={<Auth />} />
-        <Route path="/products" element={<ProductsDetail />} />
-        <Route path="/creator" element={<CreatorMain />} />
-        <Route path="/creator/mypage" element={<CreatorMypage />} />
-        <Route path="/creator/mypage/edit/*" element={<Edit />} />
       </Routes>
       <Footer />
     </BrowserRouter>
